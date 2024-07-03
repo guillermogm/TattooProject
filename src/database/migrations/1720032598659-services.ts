5aef8e71ab8e0a45561c6b39e104c12f58db95d5
guillermogm <guillermogm009@gmail.com>
2024-07-03 20:51:28 +0200

    feat: migration role

--- src/database/migrations/1720032598659-services.ts
+++ src/database/migrations/1720032598659-services.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class User1720031752744 implements MigrationInterface {
+export class Services1720032598659 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
     }
