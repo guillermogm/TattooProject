4af652e5024f3424c97a691f7cd492a4f24dacc6
guillermogm <guillermogm009@gmail.com>
2024-07-07 19:58:57 +0200

    feat: update service controller

--- src/controllers/services.controller.ts
+++ src/controllers/services.controller.ts
@@ -56,3 +56,32 @@ export const createService = async (req: Request, res: Response) => {
 
     }
 }
+
+export const updateService = async (req: Request, res: Response)=>{
+    try {
+        const idService= req.params.id
+        const newInfo=req.body
+        
+        const updatedService = await Service.update(Number(idService), newInfo)
+    
+        if(!updatedService.affected){
+            return res.status(400).json({
+                success:false,
+                message:"Service not found"
+            })
+        }
+
+        return res.status(200).json({
+            success:true,
+            message:"Service Updated successfully"
+        })
+
+    } catch (error) {
+
+        return res.status(500).json({
+            success:false,
+            message:"Error updating service"
+        })
+        
+    }
+}
