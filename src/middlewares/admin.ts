a01f3b73417352777ac172ecbaadc11c5ee3ef5a
guillermogm <guillermogm009@gmail.com>
2024-07-07 12:09:09 +0200

    feat: Folder structure and middlewares User, Admin, SuperAdmin

---
+++ src/middlewares/admin.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+
+export const admin= (req: Request,res: Response, next: NextFunction)=>{
+    try {
+
+    if (req.tokenData.role !== "admin"){
+        return res.status(401).json({
+            success:false,
+            message:"Unauthorized"
+        })
+    }
+
+    next();
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:"Error Authentication"
+        })
+    }
+
+}
---
+++ src/middlewares/user.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from 'jsonwebtoken'
+import { TokenDecoded } from "../types";
+
+export const user= (req: Request,res: Response, next: NextFunction)=>{
+    try {
+
+    if(!req.headers.authorization){
+       return res.status(401).json({
+            success:false,
+            message:"Unauthorized"
+        })
+    }
+    //spit convierte en Arrays a partir de un caracter
+    const token=req.headers.authorization.split(" ")[1]
+    const decoded= jwt.verify(token,process.env.SECRET as string) as TokenDecoded;
+
+    req.tokenData= {
+        id: decoded.id,
+        role: decoded.role
+    }
+
+    next();
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:"Authentication error"
+        })
+    }
+
+}
